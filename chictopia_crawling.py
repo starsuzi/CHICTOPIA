44be7bb8d3ac400eed6adc125d58b904bcee6fe4
starsuzi <starsuzi@naver.com>
2019-02-13 10:51:07 +0900

    made funstions

--- chictopia_crawling.py
+++ chictopia_crawling.py
@@ -4,62 +4,85 @@ import requests
 from io import BytesIO
 from PIL import Image
 
-req = requests.get('http://www.chictopia.com/photo/show/1154261-Public+Desire+ankle+boots-black-nasty-gal-top-dark-green-public-desire-boots')
-html = req.text 
+def crawler(post_url):
 
-soup = BeautifulSoup(html, 'lxml')
+    req = requests.get(post_url)
+    html = req.text 
+    soup = BeautifulSoup(html, 'lxml')
 
-photographer_and_title = soup.find('title').string.replace('"', '').split(' | ')[1].split(' by ')
+    title, photographer = get_title_photographer(soup)
+    lst_url = get_image_url(soup)
+    lst_size = get_size(lst_url)
+    lst_tag = get_tags(soup)
+    lst_item = get_items(soup)
 
-title = photographer_and_title[0]
-photographer = photographer_and_title[1]
+    return title, photographer, lst_url, lst_size, lst_tag, lst_item
 
-print(title)
-print(photographer)
+def get_title_photographer(soup):
 
-lst_url = []
+    photographer_and_title = soup.find('title').string.replace('"', '').split(' | ')[1].split(' by ')
 
-img_urls = soup.find('div', {'style':'display:inline-block'})
+    title = photographer_and_title[0]
+    photographer = photographer_and_title[1]
 
-for img_url in img_urls:
-    try:           
-        #print(img_url.get('src'))
-        lst_url.append(img_url.get('src').replace('_sm', ''))
-    except :
-        pass
+    print(title)
+    print(photographer)
+    return title, photographer
 
-print(lst_url)
+def get_image_url(soup):
+    
+    lst_url = []
 
-lst_size = []
+    img_urls = soup.find('div', {'style':'display:inline-block'})
 
-for url in lst_url:
-    image_raw = requests.get(url)
-    image = Image.open(BytesIO(image_raw.content))
-    #width, height = image.size
-    lst_size.append(image.size)
+    for img_url in img_urls:
+        try:           
+            lst_url.append(img_url.get('src').replace('_sm', ''))
+        except :
+            pass
 
-print(lst_size)
+    print(lst_url)
+    return lst_url
 
-tags = soup.find('div', {'class':'left clear px10'}).find_all('a')
+def get_size(lst_url):
 
-lst_tag = []
+    lst_size = []
 
-for tag in tags:
-    lst_tag.append(tag.string)
-print(lst_tag)
+    for url in lst_url:
+        image_raw = requests.get(url)
+        image = Image.open(BytesIO(image_raw.content))
+        #width, height = image.size
+        lst_size.append(image.size)
 
-lst_item = []
-items = soup.find_all('div', {'class':'garmentLinks left'})
+    print(lst_size)
+    return lst_size
 
-for item in items:
-    str_item = ''
-    item_names = item.find_all('a')
-    for item_name in item_names:
-        str_item = str_item + ' '+ item_name.string
-   # print(item_name)
-    str_item = str_item[1:]
-    #print(str_item)
-    lst_item.append(str_item)
+def get_tags(soup):
 
-print(lst_item)
+    lst_tag = []
+    tags = soup.find('div', {'class':'left clear px10'}).find_all('a')
 
+    for tag in tags:
+        lst_tag.append(tag.string)
+    
+    print(lst_tag)
+    return lst_tag
+
+def get_items(soup):
+
+    lst_item = []
+    items = soup.find_all('div', {'class':'garmentLinks left'})
+
+    for item in items:
+        str_item = ''
+        item_names = item.find_all('a')
+        for item_name in item_names:
+            str_item = str_item + ' '+ item_name.string
+        str_item = str_item[1:]
+        lst_item.append(str_item)
+
+    print(lst_item)
+    return lst_item
+
+crawler('http://www.chictopia.com/photo/show/1154261-Public+Desire+ankle+boots-black-nasty-gal-top-dark-green-public-desire-boots')
+crawler('http://www.chictopia.com/photo/show/1154279-black+white+and+studded-black-forever-21-boots-eggshell-sheinside-sweater-neutral-balenciaga-bag')
